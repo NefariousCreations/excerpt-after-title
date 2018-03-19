687bdb2d42a32a800d9bc1f7c32660a8312fd6fc
Corey Crowden <corey@nefariouscreations.com.au>
2018-03-20 00:14:55 +1100

    Initial commit

---
+++ excerpt-after-title.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Plugin Name: Excerpt After Title
+ * Plugin URI: https://nefariouscreations.com.au
+ * Description: A basic JS funtion to move the manual excerpt box after the title field.
+ * Version: 1.0.0
+ * Author: Nefarious Creations
+ * Author URI: https://nefariouscreations.com.au
+ */
+
+/**
+ * Move the post excerpt before the main WYSIWYG content editor and add a description.
+ */
+add_action('acf/input/admin_head', function () {
+
+  if ( ('post' == get_current_screen()->id) or ('page' == get_current_screen()->id) ) {
+    ?>
+
+    <script type="text/javascript">
+      jQuery(function ($) {
+        $(document).ready(function () {
+
+
+            /**
+             * Gutenberg Changes
+             */
+
+            /**
+             * Move the post excerpt to below the main title.
+             */
+            if ($('.gutenberg-editor-page button:contains("Excerpt")').length) {
+                // Move excerpt before main editor
+                $('.editor-post-title').after($('button:contains("Excerpt")').closest(".components-panel__body").addClass("is-opened post-excerpt").removeClass('components-panel__body'));
+                // Remove the panel button and header
+                $('.post-excerpt .components-panel__body-title').remove();
+                // Change the instruction text
+                $('.post-excerpt label').html('Write an excerpt <i>(Important)</i>');
+            }
+
+            /**
+            * Add instructions to the tags editor.
+            */
+            if ($('.gutenberg-editor-page .components-form-token-field__input-container').length) {
+                $('.components-form-token-field__input-container').append("<p class='description' style='padding:6px;'>Add tags here that describe the article. If you are utilising automated social media posts these will be used as hashtags.</p>");
+            }
+
+            /**
+             * Classic Editor Changes
+             */
+
+            /**
+             * Move the post excerpt above the main content editor.
+             */
+            if ($('body:not(.gutenberg-editor-page) #postexcerpt').length) {
+                // Add description class to default instructions
+                $('#postexcerpt .inside p').addClass('description');
+                // Add additional instructions
+                $('#postexcerpt .inside').prepend("<hr><h3>Excerpt</h3><p>This excerpt is used throughout the website, by search engines and social media. The excerpt is a brief introduction to the article that is will be seen anywhere the post / page appears, <strong>so make sure you add one</strong>.</p>");
+                // Move excerpt before main editor
+                $('#postdivrich').before($('#postexcerpt .inside').addClass('post-excerpt'));
+            }
+
+            /**
+             *  Add title before post content editor
+             */
+            $('body:not(.gutenberg-editor-page) #postdivrich').before("<hr><h3>Post Content</h3>");
+
+            /**
+             * Add instructions to the tags editor.
+             */
+            if ($('body:not(.gutenberg-editor-page) #tagsdiv-post_tag').length) {
+                $('#tagsdiv-post_tag .inside').prepend("<p class='description'>Add tags here that describe the article. If you are utilising automated social media posts these will be used as hashtags.</p>");
+            }
+
+        });
+      });
+    </script>
+
+    <style type="text/css">
+
+        /**
+        * Post Excerpt Styles (Gutenberg)
+        */
+        .gutenberg-editor-page .post-excerpt {
+          margin-bottom: 32px;
+          border-bottom: 1px solid #e2e4e7;
+        }
+        .gutenberg-editor-page .post-excerpt > div {
+          margin-left: auto;
+          margin-right: auto;
+          max-width: 732px;
+          padding: 0 62px 32px;
+        }
+        .gutenberg-editor-page .post-excerpt textarea {
+          min-height: 180px;
+          height: fit-content;
+          padding: 12px 14px;
+          font-size: 16px !important;
+          line-height: 1.65 !important;
+        }
+        /**
+         * Post Excerpt Styles (Classic)
+         */
+        body:not(.gutenberg-editor-page) #postexcerpt {
+          display: none;
+        }
+        body:not(.gutenberg-editor-page) #excerpt {
+          min-height: 10em;
+        }
+
+    </style>
+
+    <?php
+  }
+});
